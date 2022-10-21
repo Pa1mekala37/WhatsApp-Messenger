95fb43da4d00195ca4becfe552f76b68f56f89aa
Pavan Mekala <pavanmekala64@gmail.com>
2022-10-21 20:29:11 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let userInputElement = document.getElementById("userInput")
+
+function linkOpen() {
+    let userInputValue = userInputElement.value;
+    if (userInputValue.length < 10) {
+        alert("Enter Valid Mobile Number");
+    }
+    if (userInputValue.length > 10) {
+        alert("Enter Valid Mobile Number");
+    }
+    if (userInputValue.length === 10) {
+        let openlink = "https://wa.me/" + "+91" + userInputValue;
+        window.open(openlink, '_blank').focus();
+    }
+}
